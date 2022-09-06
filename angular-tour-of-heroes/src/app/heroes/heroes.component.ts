c60bde49a96a4927511e0bb500bb2ab5e9e8d2a1
mat-ilde <pascualsoto.matilde@gmail.com>
2022-09-06 12:04:16 +0100

    List of heroes done

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes'
+
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
+  selectedHero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 
 }
